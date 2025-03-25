59a6c8d5c1f71a44a414ddfe57171f812b229bc2
narmatha200323 <narmathanarmatha2338@gmail.com>
2025-03-24 23:02:07 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE>
+<html>
+<head>
+<title>Cloud Computing</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" type="text/css" href="css/main.css" />
+<link rel="shortcut icon" type="image/x-icon" href="images/logo.gif" />
+</head>
+<body>
+<div id="content">
+  <div id="header"></div>  
+  <ul id="menu">
+    <li><a class="current" href="index.php">Home</a></li>
+    <li><a href="Seller.php">Seller</a></li>
+    <li><a href="Server.php">Server</a></li>
+    <li><a href="Bank.php">Bank</a></li>
+    <li><a href="User.php">User</a></li>   
+  </ul>
+  <div style="clear: both;"></div>
+  <div class="third1">
+    <h2><center>Time Analysis</center></h2>
+   	 <img src="images/c3.png" width="279" height="169"/>
+  </div>
+  <div class="third2">
+    <h2><center></center></h2>
+   	<img src="images/c1.jpg" width="619" height="311"/>
+  </div>
+  <div style="clear: both;"></div>
+</div>
+<div id="prefooter">
+  <p align="center"></p>
+</div>
+<div id="footer">  
+  <p align="center"></p>
+</div>
+</body>
+</html>
